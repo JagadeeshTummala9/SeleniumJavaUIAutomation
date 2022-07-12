467c1df0fa8495d6c33010bd1ae821eacd445a15
JagadeeshTummala9 <jagadeeshtummala9@gmail.com>
2022-07-12 17:13:11 +0530

    Added testng

--- src/test/java/SampleTest.java
+++ src/test/java/SampleTest.java
@@ -2,10 +2,12 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
 
 public class SampleTest {
 
-    public static void main(String[] args) {
+    @Test
+    public void test() {
 
         // System Property for Chrome Driver
         System.setProperty("webdriver.chrome.driver", "/Users/jagadeeshtummala/Downloads/chromedriver");
